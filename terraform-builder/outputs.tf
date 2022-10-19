85b0d64b7c2ab7f60062114f3c2ded8716a3906b
Jacob McSwain <jacob@mcswain.dev>
2022-10-19 01:23:47 -0500

    Add CI and build pipeline
    Change-Id: Ib5c3d9f2fd3906f4f7093504918b7a4b4354b3c4

---
+++ terraform-builder/outputs.tf
@@ -0,0 +1,39 @@
+output "ssh_private_key" {
+  sensitive = true
+  value     = tls_private_key.build_key.private_key_pem
+}
+
+output "ssh_public_key" {
+  value = tls_private_key.build_key.public_key_pem
+}
+
+output "instance_ips" {
+  sensitive = true
+  value = [
+    for inst in aws_instance.builder : inst.public_ip
+  ]
+}
+
+output "s3_outputs" {
+  value = [
+    for arch in var.architectures_to_build : "webview-${var.chrome_version}-${arch}.apk.xz"
+  ]
+}
+
+output "ci_aws_access_key_id" {
+  sensitive = true
+  value     = aws_iam_access_key.ci.id
+}
+
+output "ci_aws_secret_access_key" {
+  sensitive = true
+  value     = aws_iam_access_key.ci.secret
+}
+
+output "bucket_name" {
+  value = local.bucket_name
+}
+
+output "chrome_version" {
+  value = var.chrome_version
+}
---
+++ terraform-builder/user-data.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -eE
+trap 'sudo shutdown -P now' ERR
+
+sudo apt-get -y -o DPkg::Lock::Timeout=300 update
+sudo apt-get -y -o DPkg::Lock::Timeout=300 install fpart
+
+sudo curl -fSsL https://github.com/in4it/tee2cloudwatch/releases/download/0.0.3/tee2cloudwatch-linux-amd64 -o /usr/bin/tee2cloudwatch
+sudo chmod +x /usr/bin/tee2cloudwatch
+
+exec > >(tee2cloudwatch -logGroup ${log_group_name} -region ${region}) 2>&1
+
+# /dev/nvme1n1 is the source volume
+sudo mkdir -p /mnt/src
+sudo mount /dev/nvme1n1 /mnt/src
+
+# Use mdadm to create a RAID-0 array of /dev/nvme2n1 and /dev/nvme3n1
+# and mount it at /mnt/md0
+sudo mdadm --create --verbose /dev/md0 --level=0 --raid-devices=2 /dev/nvme2n1 /dev/nvme3n1
+sudo mkfs.ext4 -F /dev/md0
+sudo mkdir -p /mnt/md0
+sudo mount /dev/md0 /mnt/md0
+
+# Allow any user to work in /mnt/md0 and /mnt/src
+sudo chmod a+wrx /mnt/src
+sudo chmod a+wrx /mnt/md0
+
+cd /mnt/md0
+
+# Bury the return code, because Chromium tree has some non-resolvable links
+time fpsync -o '-aogv --numeric-ids --chown=ubuntu:ubuntu' -n $(nproc) -vvv /mnt/src/chromium-webview /mnt/md0/chromium-webview
+
+cd chromium-webview
+
+git pull origin cr-11.0
+
+%{ for arch in architectures ~}
+./build-webview.sh -a "${arch}" -s -r "${chrome_version}:${chrome_shortversion}"
+[ "${arch}" '==' "x64" ] && android_arch="x86_64" || android_arch=${arch}
+aws s3 cp "../prebuilt/$android_arch/webview.apk.xz" "s3://${bucket_name}/webview-${chrome_version}-${arch}.apk.xz"
+%{ endfor ~}
+
+sudo shutdown -P now
